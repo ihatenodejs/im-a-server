b7919f02b0196eba764beecfa93259df5d654a0b
Aidan <aidan@p0ntus.com>
2025-04-14 00:04:53 -0400

    lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="w-full max-w-md text-center">
         <h1 className="text-4xl font-bold">Hi there,</h1>
-        <p className="text-2xl mt-1">My name is <span className="text-slate-500">{process.env.NEXT_PUBLIC_SERVER_NAME}</span>, and I'm a server.</p>
+        <p className="text-2xl mt-1">My name is <span className="text-slate-500">{process.env.NEXT_PUBLIC_SERVER_NAME}</span>, and I&apos;m a server.</p>
       </div>
       <div className="w-full max-w-md text-center my-10">
         <h1 className="text-3xl font-bold mb-4">I am the backbone for</h1>
