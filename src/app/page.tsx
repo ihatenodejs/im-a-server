f9f6eb04877ca7ab42ac5fea0537ed0de80da685
Aidan <aidan@p0ntus.com>
2025-04-23 12:46:08 -0400

    design: add shadcn, switch to cards (what a surprise)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
-'use client'
-
 import Services from "./components/services"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="w-full max-w-md text-center">
-        <h1 className="text-4xl font-bold">Hi there,</h1>
-        <p className="text-2xl mt-1">My name is <span className="text-slate-500">{process.env.NEXT_PUBLIC_SERVER_NAME}</span>, and I&apos;m a server.</p>
-      </div>
-      <div className="w-full max-w-md text-center my-10">
-        <h1 className="text-3xl font-bold mb-2">I am the backbone for</h1>
-        <Services />
-      </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-4xl font-bold">
+            I'm <span className="text-muted-foreground">{process.env.NEXT_PUBLIC_SERVER_NAME || "a server"}</span>.
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="-mt-4">
+          Welcome to my little slice of the internet.
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">
+            I'm running...
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Services />
+        </CardContent>
+      </Card>
     </div>
   )
 }
